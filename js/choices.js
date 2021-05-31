91218c38d4e9bfa2b918385d6c5e5f8edc501744
vasy1234 <spec_2017@mail.ru>
2021-05-31 20:21:59 +0500

    Weblayout blanchard

---
+++ js/choices.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', function () {
+  const element = document.querySelector(".select-1");
+  const choices = new Choices(element, {
+    searchEnabled: false,
+    searchChoices: false,
+  });
+});
+window.addEventListener('DOMContentLoaded', function () {
+  const element = document.querySelector(".select-2");
+  const choices = new Choices(element, {
+    searchEnabled: false,
+    searchChoices: false,
+  });
+});
+window.addEventListener('DOMContentLoaded', function () {
+  const element = document.querySelector(".select-3");
+  const choices = new Choices(element, {
+    searchEnabled: false,
+    searchChoices: false,
+  });
+});
+window.addEventListener('DOMContentLoaded', function () {
+  const element = document.querySelector(".select-4");
+  const choices = new Choices(element, {
+    searchEnabled: false,
+    searchChoices: false,
+  });
+});
+window.addEventListener('DOMContentLoaded', function () {
+  const element = document.querySelector(".select-5");
+  const choices = new Choices(element, {
+    searchEnabled: false,
+    searchChoices: false,
+  });
+});
---
+++ js/swiper.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper('.swiper-container', {
+  speed: 500,
+  loop: true,
+  setInitialSlide: 0,
+  spaceBetween: 0,
+});
+// var mySwiper = document.querySelector('.swiper-container').swiper
+
+// mySwiper.slideNext();
+
+// var mySwiper = new Swiper('.swiper-container', {
+//   autoplay: {
+//     delay: 1000,
+//   },
+// });
