fe35de79a73175b053a93b36e40812e38bd843f1
verso0100@gmail.com <verso0100@gmail.com>
2021-06-19 22:46:37 +0500

    blanchard адаптив hero + header +..

--- js/menu.js
+++ js/menu.js
@@ -8,6 +8,7 @@ window.addEventListener('DOMContentLoaded', function () {
   document.querySelector('.header__burger').addEventListener('click', function () {
     document.querySelector('.nav__list-top').classList.toggle('open-menu')
     document.querySelector('.header__burger').classList.toggle('open-menu')
+    document.querySelector('.header__btn').classList.toggle('open-menu')
     // document.querySelector('body').classList.toggle('fixed-page')
   })
 })
